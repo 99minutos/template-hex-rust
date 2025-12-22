0f50bffa0f7ab4ff6a261a6b291776c257bec3be
Carlos Rafael Arreola Hernández <rafael-arreola@outlook.com>
2025-12-22 09:55:13 -0700

    feat: redis path

--- src/infrastructure/providers/prv_redis.rs
+++ src/infrastructure/providers/prv_redis.rs
@@ -3,10 +3,11 @@ use std::sync::Arc;
 
 pub struct RedisProvider {
     database: Arc<redis::Connection>,
+    prefix: String,
 }
 
 impl RedisProvider {
-    pub async fn new(url: String) -> Result<Self, redis::RedisError> {
+    pub async fn new(url: &str, prefix: &str) -> Result<Self, redis::RedisError> {
         let client = redis::Client::open(url)?;
         let mut conn = client.get_connection()?;
 
@@ -16,10 +17,15 @@ impl RedisProvider {
 
         Ok(RedisProvider {
             database: arc_client,
+            prefix: prefix.to_string(),
         })
     }
 
     pub fn get_database(&self) -> Arc<redis::Connection> {
         self.database.clone()
+    }
+
+    pub fn get_path(&self, key: &[&str]) -> String {
+        format!("{}:{}", self.prefix, key.join(":"))
     }
 }
