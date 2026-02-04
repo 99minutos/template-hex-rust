e5c1765fd194c5188f01f1b11c138a395ae0ebdb
Carlos Rafael Arreola Hernández <rafael-arreola@outlook.com>
2026-02-03 20:13:36 -0700

    feat: add IfIsHumanReadable

--- src/domain/products.rs
+++ src/domain/products.rs
@@ -1,8 +1,11 @@
 use serde::{Deserialize, Serialize};
+use serde_with::{IfIsHumanReadable, serde_as};
 
+#[serde_as]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Product {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    #[serde_as(as = "Option<IfIsHumanReadable<serde_with::DisplayFromStr>>")]
     pub id: Option<bson::oid::ObjectId>,
     pub name: String,
     pub price: f64,
