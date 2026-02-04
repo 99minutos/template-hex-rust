bf37453514bc0badae51e0e8894dc8efc11d0164
Carlos Rafael Arreola Hernández <rafael-arreola@outlook.com>
2026-02-03 22:15:26 -0700

    feat: add IfIsHumanReadable

--- src/domain/orders.rs
+++ src/domain/orders.rs
@@ -1,11 +1,12 @@
 use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
-use serde_with::serde_as;
+use serde_with::{IfIsHumanReadable, serde_as};
 
 #[serde_as]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Order {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    #[serde_as(as = "Option<IfIsHumanReadable<serde_with::DisplayFromStr>>")]
     pub id: Option<bson::oid::ObjectId>,
     pub user_id: ObjectId,
     pub product_id: ObjectId,
